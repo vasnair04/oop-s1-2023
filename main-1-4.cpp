0c828cd2cb52921f343a91397eef1cd29d98669b
vasnair04 <vasudevsnair@gmail.com>
2023-03-15 18:21:10 +1030

    first commit test

--- main-1-4.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern void print_scaled(int array[3][3],int scale);
-
-int main(){
-  int scale = 3 ;
-  int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
-  print_scaled(threebythree,scale) ;
-}
--- main-2-1.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-extern void print_binary_str(std::string decimal_number);
-
-int main() {
-  char decimal_number[3] = "13";
-  print_binary_str(decimal_number);
-}
