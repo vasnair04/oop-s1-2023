2f2eec3b6b77cbce1a57013f7fcf35a49a5acc61
vasnair04 <vasudevsnair@gmail.com>
2023-03-21 20:37:19 +1030

    i lied x2

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -5,7 +5,7 @@ PersonList deepCopyPersonList(PersonList p1) {
   PersonList p2;
   int n = p1.numPeople;
   p2.numPeople = n;
-  for (int i=n;i>0;i--) {
+  for (int i=0;i<n;i++) {
     p2.people[i].age = p1.people[i].age;
     p2.people[i].name = p1.people[i].name;
   }
--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,6 @@
 PersonList shallowCopyPersonList(PersonList p1) {
   PersonList p2;
   p2.numPeople = p1.numPeople;
-  *p2.people->name = *p1.people->name;
-  *p2.people->age = *p1.people->age;
+  *p2.people = *p1.people
   return p2;
 }
