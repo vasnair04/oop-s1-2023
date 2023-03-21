20dcb766eabc79c4e007594c268f98dd10bcadd1
vasnair04 <vasudevsnair@gmail.com>
2023-03-21 20:54:22 +1030

    i lied x4

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,6 +4,7 @@
 PersonList shallowCopyPersonList(PersonList p1) {
   PersonList p2;
   p2.numPeople = p1.numPeople;
+  p2.people = new Person[p1.numPeople];
   *p2.people = *p1.people;
   return p2;
 }
