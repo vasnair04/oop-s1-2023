6484743efe1913f2cfb589b1cd0e1600a6faf887
vasnair04 <vasudevsnair@gmail.com>
2023-03-21 20:32:46 +1030

    i lied

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,7 @@
 PersonList shallowCopyPersonList(PersonList p1) {
   PersonList p2;
   p2.numPeople = p1.numPeople;
-  *p2.people.name = *p1.people.name;
-  *p2.people.age = *p1.people.age;
+  *p2.people->name = *p1.people->name;
+  *p2.people->age = *p1.people->age;
   return p2;
 }
