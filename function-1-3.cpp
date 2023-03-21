9d9be6294822d248793c907b31d623236d3d3553
vasnair04 <vasudevsnair@gmail.com>
2023-03-21 13:18:45 +1030

    fixed qn4

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -4,7 +4,8 @@
 PersonList deepCopyPersonList(PersonList p1) {
   PersonList p2;
   p2.numPeople = p1.numPeople;
-  for (int i=0;i<p1.numPeople;i++) {
+  int n = p1.numPeople;
+  for (int i=0;i<n;i++) {
     p2.people[i].age = p1.people[i].age;
     p2.people[i].name = p1.people[i].name;
   }
--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,6 +4,7 @@
 PersonList shallowCopyPersonList(PersonList p1) {
   PersonList p2;
   p2.numPeople = p1.numPeople;
-  p2.people = p1.people;
+  p2.people->age = p1.people->age;
+  p2.people->name = p1.people->name;
   return p2;
 }
