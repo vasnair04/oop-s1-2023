d73a00aee2c0fc1b3639584648e51f6c048d8574
vasnair04 <vasudevsnair@gmail.com>
2023-03-21 20:28:14 +1030

    fixed 3 and 4 for hopefully the last time

--- Person.h
+++ Person.h
@@ -1,5 +1,6 @@
 #ifndef PERSON_H
 #define PERSON_H
+#include <iostream>
 
 struct Person {
   std::string name;
--- function-1-3.cpp
+++ function-1-3.cpp
@@ -3,8 +3,8 @@
 
 PersonList deepCopyPersonList(PersonList p1) {
   PersonList p2;
-  p2.numPeople = p1.numPeople;
   int n = p1.numPeople;
+  p2.numPeople = n;
   for (int i=0;i<n;i++) {
     p2.people[i].age = p1.people[i].age;
     p2.people[i].name = p1.people[i].name;
