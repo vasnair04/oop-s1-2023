b9b29a2c12f61869529a2d9322b1b4342acbb918
vasnair04 <vasudevsnair@gmail.com>
2023-04-27 11:46:45 +0930

    first commit 4

--- AirFleet.cpp
+++ AirFleet.cpp
@@ -1,17 +1,22 @@
 #include "AirFleet.h"
 
 AirFleet::AirFleet(){
-  fleet[0] = new AirCraft[5];
+  fleet = new AirCraft*[5];
   Airplane A1(20,10);
+  Airplane* p1 = &A1;
   Helicopter H1(10000, "BlackHawk");
+  Helicopter* p2 = &H1;
   AirCraft A2(5000);
+  AirCraft* p3 = &A2;
   Helicopter H2(100,"WhiteHawk");
+  Helicopter *p4 = &H2;
   Airplane A3(15,20);
-  fleet[0] = &A1;
-  fleet[1] = &H1;
-  fleet[2] = &A2;
-  fleet[3] = &H2;
-  fleet[4] = &A3;
+  Airplane *p5 = &A3;
+  fleet[0] = p1;
+  fleet[1] = p2;
+  fleet[2] = p3;
+  fleet[3] = p4;
+  fleet[4] = p5;  
 }
 
 AirCraft** AirFleet::get_fleet(){
@@ -19,6 +24,6 @@ AirCraft** AirFleet::get_fleet(){
 }
 
 AirFleet::~AirFleet(){
-  delete fleet;
+  delete fleet ;
 }
 
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -5,5 +5,6 @@ using namespace std;
 
 int main(void){
   AirFleet F1();
+  AirCraft* Fleet = F1.get_fleet();
   return 0;
 }
